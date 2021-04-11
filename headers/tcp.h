416563db5ca499dc01cd33e966b05c34a79148d0
Emanuel Turtula <emanuelturtula@gmail.com>
2021-04-11 16:50:25 -0300

    Remove not necessary prototypes

--- headers/tcp.h
+++ headers/tcp.h
@@ -26,8 +26,6 @@ status_t receive_tcp_message(int sockfd, T3PResponse *t3pResponse);
 status_t receive_tcp_command(int sockfd, T3PCommand *t3pCommand);
 status_t parse_tcp_message(string response, T3PResponse *t3pResponse);
 status_t poll_tcp_message(int sockfd, string *data_stream);
-status_t receive_invitation_from_timeout(int sockfd,context_t *context);
-status_t receive_invitation_from(int sockfd,context_t *context,string *invitationhost);
 status_t parse_tcp_command(string message, T3PCommand *t3pCommand);
 tcpcommand_t parse_tcp_command(string socket_message, string *argument);
 status_t poll_event(int connectedSockfd, string *stdin_message, string *socket_message);
